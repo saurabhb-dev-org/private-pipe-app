e16b7368700b4c3f9bad6adea778308162785e8f
saurabhb-gh1 <saurabhb@wearehackerone.com>
2025-08-02 15:48:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, pipeline - forked ');
+    res.send(process.env);
 });
 
 app.listen(PORT, () => {
