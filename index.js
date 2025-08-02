863928189db93057571ab4a929c2a8d93b466825
saurabhb-gh1 <saurabhb@wearehackerone.com>
2025-08-02 15:23:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send(process.env);
+    res.send(process.env.database_password);
 });
 
 app.listen(PORT, () => {
