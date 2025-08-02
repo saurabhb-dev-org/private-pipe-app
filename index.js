d2e5fd5e49a1a6831d3d467d2fe3b7d8a26f2f7e
saurabhb-gh1 <saurabhb@wearehackerone.com>
2025-08-02 15:26:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send(process.env.postgres_password);
+    res.send(process.env.github_pat);
 });
 
 app.listen(PORT, () => {
